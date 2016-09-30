6bf1f9989b95c75a1114dec689288c0e46c704d0
Le-Khoi Phong <phong.lekhoi@gmail.com>
2016-09-30 16:21:03 +0800

    simplify root since built-in devtool is removed

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,15 +1,26 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import App from './App';
+
 /**
- * Just like our store, we configure a 'Root' component that is
- * required based on the env variable. This component is typically one
- * surrounded by a <Provider>.
+ * Component is exported for conditional usage in Root.js
  */
+module.exports = class Root extends Component {
+  render() {
+    const { store } = this.props;
+    return (
+      /**
+       * Provider is a component provided to us by the 'react-redux' bindings that
+       * wraps our app - thus making the Redux store/state available to our 'connect()'
+       * calls in component hierarchy below.
+       */
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  }
+};
 
-let loadedModule = null;
-
-if (process.env.NODE_ENV === 'production') {
-  loadedModule = require('./Root.prod.js');
-} else {
-  loadedModule = require('./Root.dev.js');
-}
-
-export const Root = loadedModule;
+// if (process.env.NODE_ENV === 'production') {
+//
+// }
